121907dc6f86246f03be98f0d8dacc07c00bd814
test <test@me.org>
2022-07-24 00:06:36 -0400

    init

--- src/main/java/app/api/App.java
+++ src/main/java/app/api/App.java
@@ -3,27 +3,27 @@ package app.api;
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.annotations.QuarkusMain;
 
-//import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
-//import org.eclipse.microprofile.openapi.annotations.info.Contact;
-//import org.eclipse.microprofile.openapi.annotations.info.Info;
-//import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
-//import javax.ws.rs.core.Application;
-//@OpenAPIDefinition(
-//        tags ={
-//                @Tag(name="org-api", description = "This API provides access for Person(customer), Organization, Case information")
-//        },
-//        info = @Info(
-//                title = "Org API",
-//                version = "0.0.1",
-//                contact = @Contact(
-//                        name ="orgapi",
-//                        url= "http://myorg/",
-//                        email = "me.org"
-//                )
-//
-//        )
-//)
+import javax.ws.rs.core.Application;
+@OpenAPIDefinition(
+        tags ={
+                @Tag(name="org-api", description = "This API provides access for Person(customer), Organization, Case information")
+        },
+        info = @Info(
+                title = "Org API",
+                version = "0.0.1",
+                contact = @Contact(
+                        name ="orgapi",
+                        url= "http://myorg/",
+                        email = "me.org"
+                )
+
+        )
+)
 //@ApplicationPath("/mkapp/rest/v1") //TODO, make note this path here does not get appended to xxResource classes!
 @QuarkusMain
 public class App {
